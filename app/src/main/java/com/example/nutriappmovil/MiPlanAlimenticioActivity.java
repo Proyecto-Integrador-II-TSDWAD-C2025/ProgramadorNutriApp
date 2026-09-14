d587881b9272d39193060c2d3ba19b4eb8ae3d8d
Ariel Fernandez <lic.arielfernandez.hys@gmail.com>
2026-09-14 07:21:35 -0300

    Agrega MiPlanAlimenticioActivity, completa plan con 4 comidas y corrige colores del tema (light/dark)

---
+++ app/src/main/java/com/example/nutriappmovil/MiPlanAlimenticioActivity.java
@@ -0,0 +1,14 @@
+package com.example.nutriappmovil;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MiPlanAlimenticioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mi_plan_alimenticio);
+    }
+}
